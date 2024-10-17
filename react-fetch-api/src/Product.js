121784bbed3cac95a43caba5feddfd410a9b9eec
vibo777 <54715728+vibo777@users.noreply.github.com>
2024-10-17 11:09:10 +0530

    update

--- react-fetch-api/src/Product.js
+++ react-fetch-api/src/Product.js
@@ -1,12 +1,21 @@
 function Product(Props){
 
-    console.log(Props);
+    function doSometing(){
+        console.log("Yes sir what can i do for you ?");
+    }
 
     return(
         <div className="card">
             <div className="card_details">
+                <div> 
+                    <img className="product_img" src={Props.url} alt=""/>
+                </div> 
                 <h1>{Props.name}</h1>
                 <h2>{Props.price}</h2>
+                <button className="product-btn" onClick={(event)=>{
+                    console.log(event);
+                    doSometing("Vaibhav");
+                }}>Buy Now</button>
             </div>    
         </div>
     )
