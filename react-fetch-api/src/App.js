37f3351ff11d34a3eab86406432476fe6d3c358d
vibo777 <54715728+vibo777@users.noreply.github.com>
2024-11-13 10:57:12 +0530

    update

--- react-fetch-api/src/App.js
+++ react-fetch-api/src/App.js
@@ -1,25 +1,43 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css'; 
 // import ProductList from './ProductList';
 // import HeaderComponent from './HeaderComponent';
 
 function App() {
 
-  let [name,setName] = useState('');
+  // let [name,setName] = useState('');
+
+    let countries = [
+      {name:"India" , capital:"New Delhi"},
+      {name:"Australia" , capital:"Canberra"},
+      {name:"New Zealand" , capital:"Wellington"}
+    ]
 
     return(
-      <>
-        <h2>Name : {name}</h2>
+      <div className='App'>
+        {
+            countries.map((country,index)=>{
+
+              return(
+                <div className='box' key={index}>
+                  <h2>{country.name}</h2>
+                  <h3>{country.capital}</h3>
+                </div>
+              )
+            })
+        }
+
+        {/* <h2>Name : {name}</h2>
         <input type="text" onChange={(event)=>{
           setName(event.target.value);
-        }} placeholder='Enter your name'/>
+        }} placeholder='Enter your name'/> */}
        
 
 
         {/* <h1>Welcome Everyone !!!</h1>
         <HeaderComponent/> 
         <ProductList/> */}
-      </>
+      </div>
     )
 }
 
--- react-fetch-api/src/ProductList.js
+++ react-fetch-api/src/ProductList.js
@@ -4,6 +4,7 @@ function ProductList(){
 
     return(
         <div className="product_list">
+          
           <Product name="JBL GO 3" price="1200 RS" url="https://in.jbl.com/dw/image/v2/BFND_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw5bf276ec/JBL_GO_3_HERO_BLUE_0077_1605x1605px.png?sw=535&sh=535"/>
 
           <Product name="Nike Shoes" price="2000 RS" url="https://static.nike.com/a/images/t_default/b14aba9a-f828-45d3-9607-b687b884aa7d/NIKE+REVOLUTION+7+EASYON.png"/>
@@ -11,7 +12,6 @@ function ProductList(){
           <Product name="Titan Watch" price="10000 RS" url="https://rukminim2.flixcart.com/image/612/612/xif0q/watch/t/6/e/-original-imagqfwchgp7yw8f.jpeg?q=70"/>
           
           <Product name="Polo T-shirt" price="547 RS" url="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxozjlkOGc8f6AdskUoxspVV45cT3penWoi1cSl2WVRuUeAIB3mg9SkXqb8gmULDUbdCs4acyCKCafWjKkqY6nAzmwAFlkN04mqe7VzSce"/>
-       
        
         </div>
     )
