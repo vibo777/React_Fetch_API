d1d49d230369e4cf49517af1af4e94b1ea85a096
vibo777 <54715728+vibo777@users.noreply.github.com>
2024-11-14 02:03:52 +0530

    update

--- react-fetch-api/src/App.js
+++ react-fetch-api/src/App.js
@@ -1,43 +1,26 @@
 //import { useState } from 'react';
 import './App.css'; 
-// import ProductList from './ProductList';
-// import HeaderComponent from './HeaderComponent';
+import ProductList from './ProductList';
+import HeaderComponent from './HeaderComponent';
 
 function App() {
 
   // let [name,setName] = useState('');
 
-    let countries = [
-      {name:"India" , capital:"New Delhi"},
-      {name:"Australia" , capital:"Canberra"},
-      {name:"New Zealand" , capital:"Wellington"}
-    ]
+    // let countries = [
+    //   {name:"India" , capital:"New Delhi"},
+    //   {name:"Australia" , capital:"Canberra"},
+    //   {name:"New Zealand" , capital:"Wellington"}
+    // ]
 
     return(
-      <div className='App'>
-        {
-            countries.map((country,index)=>{
+   
 
-              return(
-                <div className='box' key={index}>
-                  <h2>{country.name}</h2>
-                  <h3>{country.capital}</h3>
-                </div>
-              )
-            })
-        }
-
-        {/* <h2>Name : {name}</h2>
-        <input type="text" onChange={(event)=>{
-          setName(event.target.value);
-        }} placeholder='Enter your name'/> */}
-       
-
-
-        {/* <h1>Welcome Everyone !!!</h1>
+      <div>
+        <h1>Welcome Everyone !!!</h1>
         <HeaderComponent/> 
-        <ProductList/> */}
-      </div>
+        <ProductList/> 
+     </div>
     )
 }
 
--- react-fetch-api/src/ProductList.js
+++ react-fetch-api/src/ProductList.js
@@ -2,17 +2,22 @@ import Product from "./Product";
 
 function ProductList(){
 
+    let products = [
+      {id:'1', name:'JBL GO 3', price:'1200 RS', url:'https://in.jbl.com/dw/image/v2/BFND_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw5bf276ec/JBL_GO_3_HERO_BLUE_0077_1605x1605px.png?sw=535&sh=535'},
+      {id:'2', name:'Nike Shoes', price:'2000 RS', url:'https://static.nike.com/a/images/t_default/b14aba9a-f828-45d3-9607-b687b884aa7d/NIKE+REVOLUTION+7+EASYON.png'},
+      {id:'3', name:'Titan Watch', price:'10000 RS', url:'https://rukminim2.flixcart.com/image/612/612/xif0q/watch/t/6/e/-original-imagqfwchgp7yw8f.jpeg?q=70'},
+      {id:'4', name:'Polo T-shirt', price:'547 RS', url:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxozjlkOGc8f6AdskUoxspVV45cT3penWoi1cSl2WVRuUeAIB3mg9SkXqb8gmULDUbdCs4acyCKCafWjKkqY6nAzmwAFlkN04mqe7VzSce'}
+    ]
+
     return(
         <div className="product_list">
-          
-          <Product name="JBL GO 3" price="1200 RS" url="https://in.jbl.com/dw/image/v2/BFND_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw5bf276ec/JBL_GO_3_HERO_BLUE_0077_1605x1605px.png?sw=535&sh=535"/>
-
-          <Product name="Nike Shoes" price="2000 RS" url="https://static.nike.com/a/images/t_default/b14aba9a-f828-45d3-9607-b687b884aa7d/NIKE+REVOLUTION+7+EASYON.png"/>
-          
-          <Product name="Titan Watch" price="10000 RS" url="https://rukminim2.flixcart.com/image/612/612/xif0q/watch/t/6/e/-original-imagqfwchgp7yw8f.jpeg?q=70"/>
-          
-          <Product name="Polo T-shirt" price="547 RS" url="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxozjlkOGc8f6AdskUoxspVV45cT3penWoi1cSl2WVRuUeAIB3mg9SkXqb8gmULDUbdCs4acyCKCafWjKkqY6nAzmwAFlkN04mqe7VzSce"/>
-       
+          {
+              products.map((product)=>{
+                return(
+                  <Product name={product.name} price={product.price} url={product.url} key={product.id} />
+                )  
+              })
+          }     
         </div>
     )
 }
